9171bea67babb4fd52846963b933809b4e7217e4
nakagawa-ren <hh.hakoware@gmail.com>
2021-06-10 12:09:55 +0000

    index-kousin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'top' => 'homes#top'
-  resources :books
+  get 'books' => 'books#index'
+  get 'books/new' => 'books#new'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as:'book'
+  get 'books/:id/edit' => 'books#edit', as:'edit_book'
+  patch 'books/:id' => 'books#update', as:'update_book'
+  delete 'books/:id' => 'books#destroy', as:'delete_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
