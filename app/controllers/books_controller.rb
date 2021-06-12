97d3a5c82135e59b5ee96728b17522446f083c87
nakagawa-ren <hh.hakoware@gmail.com>
2021-06-12 05:12:40 +0000

    edit-new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,23 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book =Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+        flash[:succes] = "Book was successfully created."
+        redirect_to book_path(@book)
+    else
+      render :new
+    end
+
   end
 
   def edit
@@ -22,19 +25,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    flash[:succes] = "Book was successfully update."
+    redirect_to book_path(@book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:succes] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
   private
     def book_params
-      params.permit(:title,:body)
+      params.require(:book).permit(:title,:body)
     end
 end
